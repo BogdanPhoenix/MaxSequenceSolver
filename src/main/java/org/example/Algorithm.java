65cb73a05e99d1e3746458e643f67e8f26017b27
BogdanPhoenix <bohdan.musco@gmail.com>
2024-11-24 13:39:35 +0200

    Version 1.2: Implementation of the algorithm to work when there are several numbers in the set that have the same first two digits

--- src/main/java/org/example/Algorithm.java
+++ src/main/java/org/example/Algorithm.java
@@ -1,24 +1,38 @@
 package org.example;
 
+import lombok.NonNull;
+
 import java.util.*;
 
 public final class Algorithm {
     private Algorithm() {}
 
     public static String getLargestDigitalPuzzle(Collection<String> numbers) {
-        Map<String, Puzzle> puzzleMap = new HashMap<>();
-
-        for(String number : numbers) {
-            Puzzle puzzle = new Puzzle(number);
-            puzzleMap.put(puzzle.getStartNum(), puzzle);
-        }
-
+        Map<String, List<Puzzle>> puzzleMap = createPuzzleMap(numbers);
         Collection<Puzzle> longest = getLongestSequence(puzzleMap);
 
         return Puzzle.concatPuzzles(longest);
     }
 
-    private static Collection<Puzzle> getLongestSequence(Map<String, Puzzle> puzzleMap) {
+    private static Map<String, List<Puzzle>> createPuzzleMap(@NonNull Collection<String> numbers) {
+        Map<String, List<Puzzle>> puzzleMap = new HashMap<>();
+
+        for(String number : numbers) {
+            Puzzle puzzle = new Puzzle(number);
+
+            if(puzzleMap.containsKey(puzzle.getStartNum())) {
+                puzzleMap.get(puzzle.getStartNum()).add(puzzle);
+            } else {
+                List<Puzzle> puzzleList = new ArrayList<>();
+                puzzleList.add(puzzle);
+                puzzleMap.put(puzzle.getStartNum(), puzzleList);
+            }
+        }
+
+        return puzzleMap;
+    }
+
+    private static Collection<Puzzle> getLongestSequence(Map<String, List<Puzzle>> puzzleMap) {
         Map<String, Collection<Puzzle>> cache = new HashMap<>();
         Collection<Puzzle> longest = new LinkedList<>();
 
@@ -34,31 +48,39 @@ public final class Algorithm {
     }
 
     private static Collection<Puzzle> createSequence(
-            Map<String, Puzzle> puzzleMap,
-            Puzzle puzzle,
+            Map<String, List<Puzzle>> puzzleMap,
+            List<Puzzle> puzzles,
             Map<String, Collection<Puzzle>> cache,
             Set<String> visited
     ) {
-        if (cache.containsKey(puzzle.getStartNum())) {
-            return cache.get(puzzle.getStartNum());
-        }
-
-        if (visited.contains(puzzle.toString())) {
-            return List.of();
-        }
-
-        visited.add(puzzle.toString());
-
         List<Puzzle> sequence = new LinkedList<>();
-        sequence.add(puzzle);
 
-        String nextKey = puzzle.getEndNum();
-        if (puzzleMap.containsKey(nextKey)) {
-            Puzzle nextPuzzle = puzzleMap.get(nextKey);
-            sequence.addAll(createSequence(puzzleMap, nextPuzzle, cache, visited));
+        for(Puzzle puzzle : puzzles) {
+            if (cache.containsKey(puzzle.toString())) {
+                return cache.get(puzzle.toString());
+            }
+
+            if (visited.contains(puzzle.toString())) {
+                return List.of();
+            }
+
+            List<Puzzle> buffer = new LinkedList<>();
+            visited.add(puzzle.toString());
+            buffer.add(puzzle);
+
+            String nextKey = puzzle.getEndNum();
+            if (puzzleMap.containsKey(nextKey)) {
+                List<Puzzle> nextPuzzle = puzzleMap.get(nextKey);
+                buffer.addAll(createSequence(puzzleMap, nextPuzzle, cache, visited));
+            }
+
+            cache.put(puzzle.toString(), buffer);
+
+            if(buffer.size() > sequence.size()) {
+                sequence = buffer;
+            }
         }
 
-        cache.put(puzzle.getStartNum(), sequence);
         return sequence;
     }
 }
